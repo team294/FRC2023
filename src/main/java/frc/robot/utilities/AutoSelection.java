e5267982885db78234029f3fb50fd60f52de996c
BCR <57075608+brendanRose1@users.noreply.github.com>
2023-01-21 15:57:15 -0800

    Created AutoSelection
    Currently does not have any autos implemented

---
+++ src/main/java/frc/robot/utilities/AutoSelection.java
@@ -0,0 +1,69 @@
+package frc.robot.utilities;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.*;
+
+
+/**
+ * Selects the auto routine based upon input from the shuffleboard
+ */
+public class AutoSelection {
+
+	//public static final int AUTO = 0;
+	
+	private TrajectoryCache trajectoryCache;
+	private SendableChooser<Integer> autoChooser = new SendableChooser<>();
+	
+	/**
+	 * AutoSelection constructor for command group
+	 * Sets up autoPlan widget 
+	 */  	
+	public AutoSelection(TrajectoryCache trajectoryCache, FileLog log) {
+		this.trajectoryCache = trajectoryCache;
+
+		// auto selections
+		//autoChooser.setDefaultOption("Auto1", AUTO_NAME);
+		//autoChooser.addOption("Auto2", AUTO_NAME);
+	
+		// show auto selection widget on Shuffleboard
+		SmartDashboard.putData("Autonomous routine", autoChooser);
+
+		// show auto parameters on Shuffleboard
+		SmartDashboard.putNumber("Autonomous delay", 0);
+		SmartDashboard.putBoolean("Autonomous use vision", false);
+	}
+
+	/**
+	 * Gets the auto command based upon input from the shuffleboard
+	 * 
+	 * @param driveTrain The driveTrain that will be passed to the auto command
+	 * @param log        The filelog to write the logs to
+	 * @return the command to run
+	 */
+	public Command getAutoCommand(DriveTrain driveTrain, FileLog log) {
+		Command autonomousCommand = null;
+
+		// Get parameters from Shuffleboard
+		int autoPlan = autoChooser.getSelected();
+
+		double waitTime = SmartDashboard.getNumber("Autonomous delay", 0);
+		waitTime = MathUtil.clamp(waitTime, 0, 15);		// make sure autoDelay isn't negative and is only active during auto
+
+		// if (autoPlan == AUTO_NAME) {
+		// 	log.writeLogEcho(true, "AutoSelect", "run Auto1");
+		// 	autonomousCommand = new AutoShootTaxi(waitTime, driveTrain, log);
+		// }
+
+		if (autonomousCommand == null) {
+			log.writeLogEcho(true, "AutoSelect", "No autocommand found");
+			autonomousCommand = new WaitCommand(1);
+		}
+
+		return autonomousCommand;
+	}
+
+}
