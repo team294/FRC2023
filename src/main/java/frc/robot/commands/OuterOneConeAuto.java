be877e21a485b2f2b9f7f0cb2e456b920140a2f9
BCR294-Laptop19 <BCR@microsoft.com>
2023-02-04 14:58:19 -0800

    Update OuterOneConeAuto.java

--- src/main/java/frc/robot/commands/OuterOneConeAuto.java
+++ src/main/java/frc/robot/commands/OuterOneConeAuto.java
@@ -22,12 +22,21 @@ public class OuterOneConeAuto extends SequentialCommandGroup {
         Trajectory oneConeTrajectory =
             TrajectoryGenerator.generateTrajectory(
                 // Start at the origin facing the +X direction
-                new Pose2d(0, 0, new Rotation2d(0)),
-                List.of(),
+                new Pose2d(0.0, 0.0, new Rotation2d(0)),
+                List.of(
+                new Translation2d(-1.956,0),
+                new Translation2d(2.277, 0.242),
+                new Translation2d(-1.956,0)
+
+                
+                
+                ),
+                new Pose2d(2.277, 0.242, new Rotation2d(0)),
                 // Pass through these two interior waypoints, making an 's' curve path
                 // List.of(new Translation2d(-1, 1), new Translation2d(2, -1)),
                 
-                new Pose2d(-4, 0, new Rotation2d(180)),
+                
+                
                 Constants.TrajectoryConstants.swerveTrajectoryConfig);
 
         var thetaController =
