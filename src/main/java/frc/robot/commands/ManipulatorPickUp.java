07efef784922c8ef3fff9fca8e214ebb427edb66
BCR <frc294@gmail.com>
2023-02-18 18:11:11 -0800

    Added Manipulator Commands for Methods

---
+++ src/main/java/frc/robot/commands/ManipulatorPickUp.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Manipulator;
+import frc.robot.utilities.FileLog;
+
+public class ManipulatorPickUp extends CommandBase {
+  /** Creates a new ManipulatorStopMotor. */
+  private final Manipulator manipulator;
+  private final FileLog log;
+
+  public ManipulatorPickUp(Manipulator manipulator, FileLog log) {
+    this.manipulator = manipulator;
+    this.log = log;
+
+    addRequirements(manipulator);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    manipulator.setMotorPercentOutput(0.5);
+    log.writeLog(false, "ManipulatorPickUp", "Initialize");
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    manipulator.stopMotor();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
