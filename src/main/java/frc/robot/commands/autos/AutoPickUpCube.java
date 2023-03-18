7997030b594a37314592666a851a35f9ce9ea12a
Don Sawdai <dsawdai@ieee.org>
2023-03-18 01:02:51 -0700

    Re-wrote AutoPickUpCube for parallel speed and to ensure cube gets to manipulator

--- src/main/java/frc/robot/commands/autos/AutoPickUpCube.java
+++ src/main/java/frc/robot/commands/autos/AutoPickUpCube.java
@@ -5,9 +5,11 @@
 package frc.robot.commands.autos;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.ManipulatorConstants;
 import frc.robot.Constants.SwerveConstants;
 import frc.robot.Constants.TrajectoryConstants;
@@ -39,35 +41,59 @@ public class AutoPickUpCube extends SequentialCommandGroup {
       Intake intake, Elevator elevator, Wrist wrist, Manipulator manipulator, 
       DriveTrain driveTrain, LED led, FileLog log) {
 
+    // Original code after refactoring:
+
+    // addCommands(
+    //   new ParallelDeadlineGroup(
+    //     new DriveToPose(posCube, SwerveConstants.kNominalSpeedMetersPerSecond, SwerveConstants.kMaxRetractingAccelerationMetersPerSecondSquare,
+    //       TrajectoryConstants.interimPositionErrorMeters, TrajectoryConstants.interimThetaErrorDegrees,false, driveTrain, log).until(() -> manipulator.isCubePresent()),
+    //     new SequentialCommandGroup(
+    //       new ElevatorWristStow(elevator, wrist, log),
+    //       new IntakeExtendAndTurnOnMotors(manipulator, intake, wrist, elevator, led, log)
+    //     )
+    //   ),
+    //   new ManipulatorSetPercent(ManipulatorConstants.pieceHoldPct, manipulator, log),
+
+    //   new ParallelCommandGroup(
+    //     new IntakeRetractAndTurnOffMotors(intake, elevator, log),
+    //     new DriveToPose(posNext, SwerveConstants.kFullSpeedMetersPerSecond, SwerveConstants.kFullAccelerationMetersPerSecondSquare,
+    //       TrajectoryConstants.interimPositionErrorMeters, TrajectoryConstants.interimThetaErrorDegrees, false, driveTrain, log),
+    //     new ConditionalCommand(
+    //       new ElevatorWristMoveToUpperPosition(ElevatorPosition.scoreLow.value, WristAngle.upperLimit.value, elevator, wrist, intake, log),
+    //       new WaitCommand(0.01),
+    //       () -> setScoreLowAtEnd
+    //     )
+    //   )
+    // );
+
     addCommands(
       new ParallelDeadlineGroup(
-        new DriveToPose(posCube, SwerveConstants.kNominalSpeedMetersPerSecond, SwerveConstants.kMaxRetractingAccelerationMetersPerSecondSquare,
-          TrajectoryConstants.interimPositionErrorMeters, TrajectoryConstants.interimThetaErrorDegrees,false, driveTrain, log).until(() -> manipulator.isCubePresent()),
-        new SequentialCommandGroup(
-          new ElevatorWristStow(elevator, wrist, log),
-          new IntakeExtendAndTurnOnMotors(manipulator, intake, wrist, elevator, led, log)
-        )
-      ),
-      new ManipulatorSetPercent(ManipulatorConstants.pieceHoldPct, manipulator, log)
-    );
 
-    if (setScoreLowAtEnd) {
-      addCommands(
-        new ParallelCommandGroup(
-          new IntakeRetractAndTurnOffMotors(intake, elevator, log),
-          new DriveToPose(posNext, SwerveConstants.kFullSpeedMetersPerSecond, SwerveConstants.kFullAccelerationMetersPerSecondSquare,
-            TrajectoryConstants.interimPositionErrorMeters, TrajectoryConstants.interimThetaErrorDegrees, false, driveTrain, log),
-          new ElevatorWristMoveToUpperPosition(ElevatorPosition.scoreLow.value, WristAngle.upperLimit.value, elevator, wrist, intake, log)
-        )
-      );
-    } else {
-      addCommands(
-        new ParallelCommandGroup(
-          new IntakeRetractAndTurnOffMotors(intake, elevator, log),
+        new SequentialCommandGroup(
+          // Drive to get cube
+          new DriveToPose(posCube, SwerveConstants.kNominalSpeedMetersPerSecond, SwerveConstants.kMaxRetractingAccelerationMetersPerSecondSquare,
+            TrajectoryConstants.interimPositionErrorMeters, TrajectoryConstants.interimThetaErrorDegrees,false, driveTrain, log),
+          // Drive to next position
           new DriveToPose(posNext, SwerveConstants.kFullSpeedMetersPerSecond, SwerveConstants.kFullAccelerationMetersPerSecondSquare,
             TrajectoryConstants.interimPositionErrorMeters, TrajectoryConstants.interimThetaErrorDegrees, false, driveTrain, log)
+        ),
+
+        new SequentialCommandGroup(
+          new ElevatorWristStow(elevator, wrist, log),
+
+          // This next sequence sets the manipulator in cube mode, extends the intake,
+          // waits until is sees a cube, then retracts the intake and puts the manipulator in "hold cube speed"
+          new IntakeExtendAndTurnOnMotors(manipulator, intake, wrist, elevator, led, log),
+
+          new ConditionalCommand(
+            new ElevatorWristMoveToUpperPosition(ElevatorPosition.scoreLow.value, WristAngle.upperLimit.value, elevator, wrist, intake, log),
+            new WaitCommand(0.01),
+            () -> setScoreLowAtEnd
+          )
         )
-      );      
-    }
+
+      )
+    );
+
   }
 }
