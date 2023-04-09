c628080db67d83ca7239c3ae1eaa1b7103beb91d
Grif <emailgriffinnow@gmail.com>
2023-04-09 16:12:15 -0700

    changed wrist profile

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -236,8 +236,8 @@ public final class Constants {
         public static final double kWristGearRatio = (50.0 / 1.0);       // From CAD, should be 50:1.  Gear reduction ratio between Falcon and gear driving the wrist (planetary and chain gears)
         public static final double kWristDegreesPerTick =  360.0 / kEncoderCPR / kWristGearRatio * 0.9726;      // CALIBRATED (fudge factor 0.9726)
 
-        public static final double kMaxAngularAcc = 1.0; // radians per second squared // TODO calibrate please
-        public static final double kMaxAngularVel = 1.0; // radians per second // TODO calibrate please
+        public static final double kMaxAngularAcc = 1.0; // degrees per second squared // TODO Calibrate
+        public static final double kMaxAngularVel = 40; // degrees per second // TODO Calibrate
 
         public static final double maxUncalibratedPercentOutput = 0.05;     // CALIBRATED
         public static final double maxPercentOutput = 0.1;          // CALIBRATED
--- src/main/java/frc/robot/commands/WristSetAngleTrapezoid.java
+++ src/main/java/frc/robot/commands/WristSetAngleTrapezoid.java
@@ -9,12 +9,9 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.CoordType;
-import frc.robot.Constants.WristConstants;;
+import frc.robot.Constants.WristConstants;
+import frc.robot.Constants.WristConstants.WristAngle;
 import frc.robot.subsystems.*;
 import frc.robot.utilities.*;
 
@@ -29,12 +26,11 @@ public class WristSetAngleTrapezoid extends CommandBase {
   private double target; // goal angle
   private double currAngle;
   private double startAngle;
-  private double maxVel; // max velocity, between 0 and kMaxSpeedMetersPerSecond in Constants 
-  private double maxAccel; // max acceleration, between 0 and kMaxAccelerationMetersPerSecondSquared in Constants
+  private double maxVel; // max velocity, between 0 and kMaxAngularVel in Constants 
+  private double maxAccel; // max acceleration, between 0 and kMaxAngularAcc in Constants
   private long profileStartTime; // initial time (time of starting point)
-  private boolean regenerate;
+  // private boolean regenerate;
   private FileLog log;
-  private boolean isOpenLoop;
 
   private int accuracyCounter = 0;
 
@@ -46,22 +42,18 @@ public class WristSetAngleTrapezoid extends CommandBase {
  
   /**
    * Drives the robot straight.
-   * @param target distance to travel, in meters (+ = forward, - = backward)
-   * @param fieldRelative false = angle is relative to current robot facing,
-   *   true = angle is an absolute field angle (0 = away from drive station)
-   * @param angle angle to drive along when driving straight (+ = left, - = right)
-   * @param maxVel max velocity in meters/second, between 0 and kMaxSpeedMetersPerSecond in Constants
-   * @param maxAccel max acceleration in meters/second2, between 0 and kMaxAccelerationMetersPerSecondSquared in Constants
-   * @param regenerate true = regenerate profile each cycle (to accurately reach target distance), false = don't regenerate (for debugging)
-   * @param isOpenLoop true = feed-forward only for velocity control, false = PID feedback velocity control
-   * @param driveTrain reference to the drive train subsystem
+   * @param target Angle to go to(- is down, + is up)
+   * @param coordType relative, relative to the current angle of the wrist,
+   *   absolute, angle is absolute to the actual wrist angle of the wrist
+   * @param maxVel max angular velocity in degrees/second, between 0 and kMaxAngularVel in Constants
+   * @param maxAccel max acceleration in degrees/second2, between 0 and kMaxAngularAcc in constants
+   * @param wrist reference to the wrist subsystem
    * @param log
    */
-  public WristSetAngleTrapezoid(double target, CoordType coordType, double maxVel, double maxAccel, boolean regenerate, boolean isOpenLoop, Wrist wrist, FileLog log) {
+  public WristSetAngleTrapezoid(double target, CoordType coordType, double maxVel, double maxAccel, Wrist wrist, FileLog log) {
     this.wrist = wrist;
     this.log = log;
-    this.regenerate = regenerate;
-    this.isOpenLoop = isOpenLoop;
+    // this.regenerate = regenerate;
     this.target = target;
     this.coordType = coordType;
     this.maxVel = MathUtil.clamp(Math.abs(maxVel), 0, WristConstants.kMaxAngularVel);
@@ -77,11 +69,19 @@ public class WristSetAngleTrapezoid extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    // Calculate target angle
+    // sets limits. Also sets to relative
     if (coordType == CoordType.kAbsolute) {
-      target = target;
+      target = target - wrist.getWristAngle();
     }
-    
+    if(wrist.getWristAngle() + target > WristAngle.upperLimit.value){
+      target = WristAngle.upperLimit.value - wrist.getWristAngle();
+    }
+    else if(wrist.getWristAngle() + target < WristAngle.lowerLimit.value){
+      target = WristAngle.lowerLimit.value - wrist.getWristAngle();
+    }
+
+        // Calculate target angle
+
     tStateFinal = new TrapezoidProfileBCR.State(target, 0.0); // initialize goal state (degrees to turn)
     tStateCurr = new TrapezoidProfileBCR.State(0.0, 0.0); // initialize initial state (relative turning, so assume initPos is 0 degrees)
     tConstraints = new TrapezoidProfileBCR.Constraints(maxVel, maxAccel); // initialize velocity and accel limits
@@ -98,56 +98,40 @@ public class WristSetAngleTrapezoid extends CommandBase {
     // Update data for this iteration
     long currProfileTime = System.currentTimeMillis();
     double timeSinceStart = (double)(currProfileTime - profileStartTime) * 0.001;
-    currAngle = driveTrain.getPose().getTranslation().getDistance(startAngle);
+    currAngle = wrist.getWristAngle() - startAngle;
 
     // Get next state from trapezoid profile
     tStateNext = tProfile.calculate(timeSinceStart + 0.010);
     double targetVel = tStateNext.velocity;
     double targetAccel = tStateNext.acceleration;
 
-    // Set wheel speeds
-    // Note:  All 4 SwerveModuleStates in the desiredStates[] array point to the same SwerveModuleState object.
-    // So, they will always have the same speed, even if we only update one of the elements of the array.
-    desiredStates[0].speedMetersPerSecond = targetVel;
-    desiredStates[1].speedMetersPerSecond = targetVel;
-    desiredStates[2].speedMetersPerSecond = targetVel;
-    desiredStates[3].speedMetersPerSecond = targetVel;
-    driveTrain.setModuleStates(desiredStates, isOpenLoop); 
-    
-    // Read current module states for logging
-    SwerveModuleState[] currentStates = driveTrain.getModuleStates();
-    double linearVel = (Math.abs(currentStates[0].speedMetersPerSecond) + Math.abs(currentStates[1].speedMetersPerSecond) +
-        Math.abs(currentStates[2].speedMetersPerSecond) + Math.abs(currentStates[3].speedMetersPerSecond))/4.0;
+    wrist.setWristVelocity(targetVel);
 
-    log.writeLog(false, "WristSetAngleTrapezoid", "profile", "angT", angleTarget,
+    log.writeLog(false, "WristSetAngleTrapezoid", "profile", "angT", target,
       "posT", tStateNext.position, 
       "velT", targetVel, "accT", targetAccel,
-      "posA", currAngle, 
-      "velA", linearVel,
-      "velA-FL", currentStates[0].speedMetersPerSecond, 
-      "velA-FR", currentStates[1].speedMetersPerSecond, 
-      "velA-BL", currentStates[2].speedMetersPerSecond, 
-      "velA-BR", currentStates[3].speedMetersPerSecond
+      "posA", currAngle
     );
 
-    if(regenerate) {
-      tStateCurr = new TrapezoidProfileBCR.State(currAngle, linearVel);
-      tProfile = new TrapezoidProfileBCR(tConstraints, tStateFinal, tStateCurr);
-      profileStartTime = currProfileTime;
-    }
+    // if(regenerate) {
+      // tStateCurr = new TrapezoidProfileBCR.State(currAngle, );
+      // tProfile = new TrapezoidProfileBCR(tConstraints, tStateFinal, tStateCurr);
+      // profileStartTime = currProfileTime;
+    // }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     log.writeLog(false, "WristSetAngleTrapezoid", "End");
-    driveTrain.stopMotors();
+    wrist.setWristAngle(wrist.getWristAngle());
+    // wrist.stopMotors();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if(Math.abs(target - currAngle) < 0.0125) {
+    if(Math.abs(target - currAngle) < 3) {
       accuracyCounter++;
       log.writeLog(false, "WristSetAngleTrapezoid", "WithinTolerance", "Target Dist", target, "Actual Dist", currAngle, "Counter", accuracyCounter);
     } else {
